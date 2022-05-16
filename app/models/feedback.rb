0b9493aa50634338db4c73a8e3212368e798efb3
chrismyhub <keyoch@gmail.com>
2022-05-17 02:17:23 +1000

    feedback.rb updated with rating

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,4 +2,5 @@ class Feedback < ApplicationRecord
     belongs_to :profile
     validates :profile_id, presence: true
     validates :comments, presence: true
+    validates :rating, presence: true
 end
