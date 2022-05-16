270b7583cf9003844aaf213a334f17ba627bcb81
chrismyhub <keyoch@gmail.com>
2022-05-16 17:03:57 +1000

    Profiles controller and index page updated

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,47 @@
 class ProfilesController < ApplicationController
+
   def index
+    @profiles = Profile.all
   end
 
   def show
   end
+
+  def new
+    @profiles = Profile.new
+  end
+
+  def listing
+    @profiles = Profile.all
+  end
+
+  def create
+      profile = Profile.new(profile_params)
+      profile.save!
+      redirect_to profile
+  end
+
+  def edit
+  end
+
+  def update
+    @profile.update(profile_params)
+    redirect_to profile_path
+  end
+
+  def destroy
+    @profile.destroy
+    redirect_to profile_listing_path
+  end
+
+  private
+  
+  def set_profile
+    @profile = Profile.find(params[:id])
+  end
+
+  def profile_params
+    return params.require(:profile).permit(:name, :phone_number, :australian_state)
+  end
 end
+
