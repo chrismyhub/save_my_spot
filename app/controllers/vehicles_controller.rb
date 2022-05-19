55afa4f50b2fdcf4d0b196ce614d3a9ff768658d
chrismyhub <keyoch@gmail.com>
2022-05-19 15:18:09 +1000

    vehicle controller updated

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,7 +1,46 @@
 class VehiclesController < ApplicationController
+  before_action :set_vehicle, nly: [:show, :update, :destroy, :edit]
+  before_action :set_user, only: [:show, :destroy]
+
   def index
   end
 
   def show
   end
+
+  def create
+    @vehicle = Vehicle.create!(vehicle_params)
+    redirect_to @vehicle
+  end
+
+  def edit
+  end
+
+  def update
+    @vehicle.update(vehicle_params)
+    redirect_to @vehicle
+  end
+
+  def destroy
+    @vehicle.destroy
+    redirect_to @profile
+  end
+
+
+ private
+
+  def set_user
+    @profile_id = current_user.profile.id
+    @profile = Profile.find(params[:profile_id])
+  end
+
+  def set_vehicle
+    @request = Request.find(params[:id])
+  end
+
+  def vehicle_params
+    return params.require(:vehicle).permit(:type, :color, :license_plate, :make, :year, :comments, :profile_id, :request_id)
+  end
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'vehicles/index'
-  get 'vehicles/show'
+  resources :vehicles
 
   devise_for :users
 
@@ -17,6 +16,7 @@ Rails.application.routes.draw do
   resources :profiles do
     resources :feedbacks
     resources :requests
+    resources :vehicles
   end
 
 
