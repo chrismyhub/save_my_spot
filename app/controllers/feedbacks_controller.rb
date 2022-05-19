0dddbf51af57dc6a6079593dcc9f25896f85f65f
chrismyhub <keyoch@gmail.com>
2022-05-19 13:09:08 +1000

    added delete button to feedback comments and filtered profile edit and delete for user profiles

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,6 +1,7 @@
 class FeedbacksController < ApplicationController
+    before_action :set_feedback_profile, only: [:create, :destroy, :edit]
+    
     def create  
-        @profile = Profile.find(params[:profile_id])
         @feedback = @profile.feedbacks.build(feedback_params)
         @feedback.user_id = current_user.id
         if @feedback.save!
@@ -8,13 +9,26 @@ class FeedbacksController < ApplicationController
         else
             redirect_to @profile, notice: "did not save!"
         end
-    
+    end
+
+    def edit
+    end
+
+    def show
+        destroy
     end
 
     def destroy
+        @feedback = @profile.feedbacks.find(params[:id])
+        @feedback.destroy
+        redirect_to @profile
     end
 
     private
+
+    def set_feedback_profile
+        @profile = Profile.find(params[:profile_id])
+    end
 
     def feedback_params
         params.require(:feedback).permit(:comment, :rating, :user_id, :profile_id)
