96bbcd092c01041f4262843aee587b522b4c348f
chrismyhub <keyoch@gmail.com>
2022-05-19 15:20:53 +1000

    vehicle.rb validation added

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,4 +1,11 @@
 class Vehicle < ApplicationRecord
     belongs_to :profile, dependent: :destroy
     has_many :requests
+
+    validates :type, presence: true
+    validates :color, presence: true, length: { maximum: 3 }
+    validates :license_plate, presence: true
+    validates :make, presence: true
+    validates :year, presence: true, length: { maximum: 4 }
+    validates :comments
 end
